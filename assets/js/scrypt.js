ed3d76025801ba80c1e339f0321c6c0ad9e8f144
Asher Clawson <31211822+KingPogona@users.noreply.github.com>
2020-09-06 16:58:42 -0600

    cap number of items in search history and fix layout for small devices

--- assets/js/scrypt.js
+++ assets/js/scrypt.js
@@ -27,109 +27,18 @@ var currentTime = moment();
 
 
 
-
-function loadContent() {
-    loadedSearchHistory = JSON.parse(localStorage.getItem("weatherSearchHistory"));
-
-    if (!loadedSearchHistory) {
-        return
-    };
-
-    searchHistory = loadedSearchHistory;
-
-    console.log(searchHistory);
-
-    for (var i = searchHistory.length - 1; i > -1; i--) {
-        displayHistory(searchHistory[i])
+//           **********Start of Functions**********
+// if the history is greater than 7 items it slices the array down to 7
+// it will only display up to 7 items on the page at a time
+function capHistory() {
+    if (searchHistory.length >= 7) {
+        searchHistory = searchHistory.slice(0, 7);
     }
 
-    searchInputEl.value = searchHistory[0];
-    console.log("loadContent: " + searchHistory[0]);
-    getWeather(searchHistory[0]);
-};
-// run load content
-loadContent();
-
-// saves to local storage
-function saveContent() {
-    localStorage.setItem("weatherSearchHistory", JSON.stringify(searchHistory));
-};
-
-
-function addToHistory(searchTerm) {
-    searchHistory.unshift(searchTerm);
-    
-    console.log(searchHistory);
-
-    saveContent();
+    $('.historyItem').slice(6).remove();
 }
 
-
-function displayHistory(searchTerm) {
-    var listItemEl = document.createElement("li");
-
-    listItemEl.className = "list-group-item historyItem";
-    listItemEl.textContent = searchTerm;
-
-    searchListEl.prepend(listItemEl);
-}
-
-function historyClickHandler(event) {
-    event.preventDefault();
-
-    // var searchTerm = searchInputEl.value.trim();
-
-    getWeather(searchTerm);
-}
-
-
-function formSubmitHandler(event) {
-    event.preventDefault();
-
-    var searchTerm = searchInputEl.value.trim();
-
-    addToHistory(searchTerm);
-    displayHistory(searchTerm);
-    getWeather(searchTerm);
-}
-
-
-function getWeather(location) {
-
-    console.log("getweather: " + location);
-
-    fetch(
-        'http://www.mapquestapi.com/geocoding/v1/address?key=GAKZihaP7YzWQWAiaS5mAthRcK7FX8fF&location=' + location
-
-
-    )
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            // console.dir(response);
-
-            var lat = response.results[0].locations[0].latLng.lat
-            var lon = response.results[0].locations[0].latLng.lng
-            // console.log(lat + ' and ' + lon);
-
-            return fetch(
-                'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly&appid=aa754955e12084ca219e14374971d05a'
-
-
-            )
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (response) {
-                    // console.dir(response);
-
-                    for (var i = 0; i < 6; i++) {
-                        write5DayText(response, i)
-                    }
-                })
-        })
-}
+// gets the current date and returns it with on offset
 function getDate(offset) {
 
     var a = moment(currentTime).add(offset, 'd')
@@ -138,13 +47,8 @@ function getDate(offset) {
     return formattedTime
 }
 
+// displays weather info on the page
 function write5DayText(source, day) {
-    // valueType 
-    // daily[0].temp
-    // console.log(source);
-    // console.log(source.daily[day].temp.day);
-    // $('.temp').eq(day).text(source.daily[day].temp.day);
-    // $('.humidity').eq(day).text(source.daily[day].humidity);
 
     var date = getDate(day);
     var weather = source.daily[day].weather[0].main;
@@ -152,10 +56,6 @@ function write5DayText(source, day) {
     var humidity = source.daily[day].humidity;
 
     weather = weather.toLowerCase();
-
-
-    // temp = temp * 9 / 5 - 459.67;
-    // temp = temp.toFixed(2);
 
     // console.log(weather);
 
@@ -193,20 +93,136 @@ function write5DayText(source, day) {
     }
 }
 
+// takes the search term and requests the cordinates for it
+// it then takes that and requests the weather for the next week
+// it then calls the write5DayText function to display the data
+function getWeather(location) {
+
+    // console.log("getweather: " + location);
+
+    fetch(
+        'http://www.mapquestapi.com/geocoding/v1/address?key=GAKZihaP7YzWQWAiaS5mAthRcK7FX8fF&location=' + location
+
+
+    )
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            // console.dir(response);
+
+            var lat = response.results[0].locations[0].latLng.lat
+            var lon = response.results[0].locations[0].latLng.lng
+            // console.log(lat + ' and ' + lon);
+
+            return fetch(
+                'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly&appid=aa754955e12084ca219e14374971d05a'
+
+
+            )
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (response) {
+                    // console.dir(response);
+
+                    for (var i = 0; i < 6; i++) {
+                        write5DayText(response, i)
+                    }
+                })
+        })
+}
+
+
+//     -----Local Storage Functions-----
+function loadContent() {
+    loadedSearchHistory = JSON.parse(localStorage.getItem("weatherSearchHistory"));
+
+    if (!loadedSearchHistory) {
+        return
+    };
+
+    searchHistory = loadedSearchHistory;
+
+    capHistory();
+
+    // console.log(searchHistory);
+
+    for (var i = searchHistory.length - 1; i > -1; i--) {
+        displayHistory(searchHistory[i])
+    }
+
+    searchInputEl.value = searchHistory[0];
+    // console.log("loadContent: " + searchHistory[0]);
+    getWeather(searchHistory[0]);
+};
+// run load content
+loadContent();
+
+// saves to local storage
+function saveContent() {
+    localStorage.setItem("weatherSearchHistory", JSON.stringify(searchHistory));
+};
+
+
+//      -----Search History Functions-----
+// adds the search term to the history and runs saveContent
+function addToHistory(searchTerm) {
+    
+    searchHistory.unshift(searchTerm);
+
+    capHistory();
+
+    // console.log(searchHistory);
+
+    saveContent();
+}
+
+// adds the element to the page for the provided search term
+function displayHistory(searchTerm) {
+    var listItemEl = document.createElement("li");
+
+    listItemEl.className = "list-group-item historyItem";
+    listItemEl.textContent = searchTerm;
+
+    searchListEl.prepend(listItemEl);
+}
+
+
+//      -----Input Handlers-----
+// handles clicking on a history item
+function historyClickHandler(event) {
+    event.preventDefault();
+
+    // var searchTerm = searchInputEl.value.trim();
+
+    getWeather(searchTerm);
+}
+
+// handles clicking the search button or hitting return
+function formSubmitHandler(event) {
+    event.preventDefault();
+
+    var searchTerm = searchInputEl.value.trim();
+
+    addToHistory(searchTerm);
+    displayHistory(searchTerm);
+    getWeather(searchTerm);
+}
+
+
+
+
+//           **********Event Listeners**********
+
 userFormEl.addEventListener("submit", formSubmitHandler);
 
 $("#searchList").on("click", ".historyItem", function () {
     let text = $(this).text().trim();
 
-    
+
     searchInputEl.value = text;
-    console.log("history click: " + text);
+    // console.log("history click: " + text);
     getWeather(text);
 
 });
-// getWeather(searchTerm);
-
-
-// console.log(date0El);
-// date0El.textContent = "test";
-
